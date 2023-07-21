ebf5cd0127509360018423d2a036249cc1a9f3fb
dialloabdoulaye848 <dialloabdoulaye848@gmail.com>
2023-07-21 22:58:08 +0000

    ajoute de code

---
+++ Openclassrooms_javascript/index3.js
@@ -0,0 +1,19 @@
+//////////////
+// Travaillez sur les fonctions
+// On définit la fonction
+
+function afficherDeuxValeurs(valeur1, valeur2) {
+  console.log("Première valeur:" + valeur1);
+  console.log("Deuxième valeur:" + valeur2);
+}
+
+// On exécute la fonction
+afficherDeuxValeurs(12, 23);
+
+//////////////////
+const monTableau = [2, 3, 5, 6];
+for (let elementDeTableau of monTableau) {
+  console.log(elementDeTableau);
+}
+
+// Définissez des méthodes d'instance et des propriétés
