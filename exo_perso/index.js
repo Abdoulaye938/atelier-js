9d8e89f65b443cb464f46de7bb211b60f60732c0
dialloabdoulaye848 <dialloabdoulaye848@gmail.com>
2023-07-24 10:21:29 +0000

    ajoute de code

--- exo_perso/index.js
+++ exo_perso/index.js
@@ -38,14 +38,41 @@
 
 //--------------------4 fois ------------------------------------
 
-var programmation = [];
+// var programmation = [];
 
-while (true) {
-  var prompText = prompt("Entrer une langage de programmation");
-  if (prompText != "") {
-    programmation.push(prompText);
-  } else {
-    break;
-  }
+// while (true) {
+//   var prompText = prompt("Entrer une langage de programmation");
+//   if (prompText != "") {
+//     programmation.push(prompText);
+//   } else {
+//     break;
+//   }
+// }
+// console.log(programmation);
+
+//--------------------5 fois --------------------------------------
+
+// var langPro = [];
+
+// while (true) {
+//   var prompText = prompt("Ecrit une langage de programmation");
+//   if (prompText != "") {
+//     langPro.push(prompText);
+//   } else {
+//     break;
+//   }
+// }
+// console.log(langPro);
+
+//--------------------------6 fois -------------------------------
+
+//
+//------------------------------------partie 2 --------------------------------
+var LangagesPro = ["c++", "python", "java", "Ruby"];
+
+var prompText = prompt("Entrez le nom d'un langage de programmation");
+if (LangagesPro.indexOf(prompText) != 1) {
+  alert("Ton langage existe !");
+} else {
+  alert("Ton langage n'existe pas :'(");
 }
-console.log(programmation);
