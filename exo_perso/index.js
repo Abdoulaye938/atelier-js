9bdea03fc32966f00a249c1d8cb22a35cf16180c
dialloabdoulaye848 <dialloabdoulaye848@gmail.com>
2023-07-24 09:28:56 +0000

    ajoute de dossier

---
+++ exo_perso/index.js
@@ -0,0 +1,51 @@
+//------------------1 fois---------------------------------
+// var langagePro = [];
+
+// while (true) {
+//   var promptText = prompt("Entrez un langage de programmation");
+//   if (promptText != "") {
+//     langagePro.push(promptText);
+//   } else {
+//     break;
+//   }
+// }
+// console.log(langagePro);
+//------------------------2 fois------------------------------------
+// var LangagesPro = [];
+
+// while (true) {
+//   var prompText = prompt("Entre une langage de programmation :");
+//   if (prompText != "") {
+//     LangagesPro.push(prompText);
+//   } else {
+//     break;
+//   }
+// }
+// console.log(LangagesPro);
+
+//----------------------3 fois ----------------------------
+
+// var LangagesPro = [];
+// while (true) {
+//   var prompText = prompt("Entrer une langage de programmation");
+//   if (prompText != "") {
+//     LangagesPro.push(prompText);
+//   } else {
+//     break;
+//   }
+// }
+// console.log(LangagesPro);
+
+//--------------------4 fois ------------------------------------
+
+var programmation = [];
+
+while (true) {
+  var prompText = prompt("Entrer une langage de programmation");
+  if (prompText != "") {
+    programmation.push(prompText);
+  } else {
+    break;
+  }
+}
+console.log(programmation);
