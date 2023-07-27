148ce8be08048e3db0e62d93ad7ecbd320ca1217
dialloabdoulaye848 <dialloabdoulaye848@gmail.com>
2023-07-27 10:14:42 +0000

    dossier et code

---
+++ exo_perso/boucle.js
@@ -0,0 +1,5 @@
+const myArr = ['Laye', 'Moussa', 'Samba', 'Demba'];
+
+for (let prenom of myArr) {
+  console.log(`Bonjouur a toi ${prenom}`);
+}
--- exo_perso/index.js
+++ exo_perso/index.js
@@ -1,78 +1,107 @@
-//------------------1 fois---------------------------------
-// var langagePro = [];
+// //------------------1 fois---------------------------------
+// // var langagePro = [];
 
-// while (true) {
-//   var promptText = prompt("Entrez un langage de programmation");
-//   if (promptText != "") {
-//     langagePro.push(promptText);
-//   } else {
-//     break;
-//   }
-// }
-// console.log(langagePro);
-//------------------------2 fois------------------------------------
-// var LangagesPro = [];
+// // while (true) {
+// //   var promptText = prompt("Entrez un langage de programmation");
+// //   if (promptText != "") {
+// //     langagePro.push(promptText);
+// //   } else {
+// //     break;
+// //   }
+// // }
+// // console.log(langagePro);
+// //------------------------2 fois------------------------------------
+// // var LangagesPro = [];
 
-// while (true) {
-//   var prompText = prompt("Entre une langage de programmation :");
-//   if (prompText != "") {
-//     LangagesPro.push(prompText);
-//   } else {
-//     break;
-//   }
-// }
-// console.log(LangagesPro);
+// // while (true) {
+// //   var prompText = prompt("Entre une langage de programmation :");
+// //   if (prompText != "") {
+// //     LangagesPro.push(prompText);
+// //   } else {
+// //     break;
+// //   }
+// // }
+// // console.log(LangagesPro);
 
-//----------------------3 fois ----------------------------
+// //----------------------3 fois ----------------------------
 
-// var LangagesPro = [];
-// while (true) {
-//   var prompText = prompt("Entrer une langage de programmation");
-//   if (prompText != "") {
-//     LangagesPro.push(prompText);
-//   } else {
-//     break;
-//   }
-// }
-// console.log(LangagesPro);
+// // var LangagesPro = [];
+// // while (true) {
+// //   var prompText = prompt("Entrer une langage de programmation");
+// //   if (prompText != "") {
+// //     LangagesPro.push(prompText);
+// //   } else {
+// //     break;
+// //   }
+// // }
+// // console.log(LangagesPro);
 
-//--------------------4 fois ------------------------------------
+// //--------------------4 fois ------------------------------------
 
-// var programmation = [];
+// // var programmation = [];
 
-// while (true) {
-//   var prompText = prompt("Entrer une langage de programmation");
-//   if (prompText != "") {
-//     programmation.push(prompText);
-//   } else {
-//     break;
-//   }
-// }
-// console.log(programmation);
+// // while (true) {
+// //   var prompText = prompt("Entrer une langage de programmation");
+// //   if (prompText != "") {
+// //     programmation.push(prompText);
+// //   } else {
+// //     break;
+// //   }
+// // }
+// // console.log(programmation);
 
-//--------------------5 fois --------------------------------------
+// //--------------------5 fois --------------------------------------
 
-// var langPro = [];
+// // var langPro = [];
 
-// while (true) {
-//   var prompText = prompt("Ecrit une langage de programmation");
-//   if (prompText != "") {
-//     langPro.push(prompText);
-//   } else {
-//     break;
-//   }
-// }
-// console.log(langPro);
+// // while (true) {
+// //   var prompText = prompt("Ecrit une langage de programmation");
+// //   if (prompText != "") {
+// //     langPro.push(prompText);
+// //   } else {
+// //     break;
+// //   }
+// // }
+// // console.log(langPro);
 
-//--------------------------6 fois -------------------------------
+// //--------------------------6 fois -------------------------------
 
-//
-//------------------------------------partie 2 --------------------------------
-var LangagesPro = ["c++", "python", "java", "Ruby"];
+// //
+// //------------------------------------partie 2 --------------------------------
+// // var LangagesPro = ["c++", "python", "java", "Ruby"];
 
-var prompText = prompt("Entrez le nom d'un langage de programmation");
-if (LangagesPro.indexOf(prompText) != 1) {
-  alert("Ton langage existe !");
-} else {
-  alert("Ton langage n'existe pas :'(");
+// // var prompText = prompt("Entrez le nom d'un langage de programmation");
+// // if (LangagesPro.indexOf(prompText) != 1) {
+// //   alert("Ton langage existe !");
+// // } else {
+// //   alert("Ton langage n'existe pas :'(");
+// // }
+
+// const paragraph =
+//   "The quick brown fox jumps over the lazy dog. If the dog barked, was it really lazy?";
+
+// const searchTerm = "dog";
+// const indexOfFirst = paragraph.indexOf(searchTerm);
+
+// console.log(
+//   `The index of the first "${searchTerm}" from the beginning is ${indexOfFirst}`
+// );
+// // Expected output: "The index of the first "dog" from the beginning is 40"
+
+// console.log(
+//   `The index of the 2nd "${searchTerm}" is ${paragraph.indexOf(
+//     searchTerm,
+//     indexOfFirst + 1
+//   )}`
+// );
+// // Expected output: "The index of the 2nd "dog" is 52"
+
+//--------------------------27/07/2023-----------------------------------------
+// condition et boucle
+// for in et for of
+
+const myArr = ['Laye', 'Moussa', 'Samba', 'Demba'];
+
+for (let prenom of myArr) {
+  console.log(`Bonjouur a toi ${prenom}`);
 }
