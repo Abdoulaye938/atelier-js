9f137540be761e110043f6647118c82b5263dfae
dialloabdoulaye848 <dialloabdoulaye848@gmail.com>
2023-08-12 22:51:18 +0000

    ajoute dossier

--- exo_perso/index.js
+++ exo_perso/index.js
@@ -1,7 +1,27 @@
 //
-let x = 5;
-if (x > 6) {
-  alert("c'est vrai");
+// let x = '5';
+// let y = 5;
+// if (x === y) {
+//   alert("c'est vrai");
+// } else {
+//   alert("c'est faux");
+// }
+let x = -10;
+
+if (x > 1) {
+  alert('x contient une valeur strictement supérieure à 1');
+} else if (x == 1) {
+  alert('x contient une valeur de 1');
+} else if (x < 1) {
+  alert('x contient une valeur strictement inferieur a 1');
+} else if (x < 0) {
+  alert('x contient une valeur strictement inferieur a 0');
 } else {
-  alert("c'est faux");
+  alert('x est negatif');
 }
+
+// } else if (x == 1) {
+//   alert('x contient la valeur 1');
+// } else {
+//   alert('x contient une valeur strictement inférieure à 1');
+// }
