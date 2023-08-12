111e68e3b0b82573f037f0a0a711d91c39b13c20
dialloabdoulaye848 <dialloabdoulaye848@gmail.com>
2023-08-12 16:08:44 +0000

    ajout

--- tache_js/02_js/exercice.js
+++ tache_js/02_js/exercice.js
@@ -1,35 +1,35 @@
 //-----------EXERCICE 1----------
 
 // Utilisation de l’opérateur typeof pour trouver le type de variable
-var s1 = 'hello';
-var n1 = 120;
-var n1 = 11.1234;
-var b1 = true;
-var x;
-var u = undefined;
-var o1 = null;
-var o2 = { id: 1, value: 200 };
-var o3 = [1, 2, 3];
-var f = function () {
-  return 1 + 2;
-};
+// var s1 = 'hello';
+// var n1 = 120;
+// var n1 = 11.1234;
+// var b1 = true;
+// var x;
+// var u = undefined;
+// var o1 = null;
+// var o2 = { id: 1, value: 200 };
+// var o3 = [1, 2, 3];
+// var f = function () {
+//   return 1 + 2;
+// };
 
-console.log(typeof s1);
-console.log(typeof n1);
-console.log(typeof n1);
-console.log(typeof b1);
-console.log(typeof x);
-console.log(typeof u);
-console.log(typeof o1);
-console.log(typeof o2);
-console.log(typeof o3);
-console.log(typeof f);
+// console.log(typeof s1);
+// console.log(typeof n1);
+// console.log(typeof n1);
+// console.log(typeof b1);
+// console.log(typeof x);
+// console.log(typeof u);
+// console.log(typeof o1);
+// console.log(typeof o2);
+// console.log(typeof o3);
+// console.log(typeof f);
 
 ///////////////////////////
 //Utilisation de l’opérateur typeof dans les contrôles conditionnels
 const x = 1;
 const y = 2;
-// let z = 'x' + 'y';
+const z = 'x' + 'y';
 console.log(x);
 console.log(typeof x);
 console.log(y);
