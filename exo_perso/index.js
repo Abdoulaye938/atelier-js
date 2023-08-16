e9b2cae107a300f6c43faa8c7e66e30369d94e12
dialloabdoulaye848 <dialloabdoulaye848@gmail.com>
2023-08-16 20:07:49 +0000

    ajoute de dossier video

--- exo_perso/index.js
+++ exo_perso/index.js
@@ -6,6 +6,7 @@
 // } else {
 //   alert("c'est faux");
 // }
+/*
 let x = -10;
 
 if (x > 1) {
@@ -19,9 +20,61 @@ if (x > 1) {
 } else {
   alert('x est negatif');
 }
+*/
 
 // } else if (x == 1) {
 //   alert('x contient la valeur 1');
 // } else {
 //   alert('x contient une valeur strictement inférieure à 1');
 // }
+
+// let i = 0;
+// while (i < 10) {
+//   console.log('BONJOUR' + i);
+//   i = i + 1;
+// }
+/*
+const notes = [12, 13, 15, 19];
+for (let i = 0; i < notes.length; i++) {
+  console.log(notes[i]);
+  console.log(i);
+}*/
+
+//
+// function quantité(selectObject) {
+//   let qtéSélectionnée = 0;
+//   for (let i = 0; i < selectObject.options.length; i++) {
+//     if (selectObject.options[i].selected) {
+//       qtéSélectionnée++;
+//     }
+//   }
+//   return qtéSélectionnée;
+// }
+
+// let btn = document.getElementById('btn');
+// btn.addEventListener('click', function () {
+//   alert(
+//     "Nombre d'options choisies : " + quantité(document.selectForm.typesMusique)
+//   );
+// });
+
+// let btns = document.getElementsByClassName('btn');
+// for (let i = 0; i < btns.length; i++) {
+//   btns[i].addEventListener('click', function () {
+//     let buttonStyle = getComputedStyle(this);
+//     let buttonBgColor = buttonStyle['backgroundColor'];
+//     document.body.style.backgroundColor = buttonBgColor;
+//   });
+// }
+
+// JavaScript peut modifier le contenu HTML
+let para = document.getElementById('para');
+// alert(para.innerHTML);
+// para.innerHTML = 'Bonjour coach je suis Bakeliste en programmation';
+
+// JavaScript peut modifier les styles HTML (CSS)
+// para.style.color = 'red';
+// para.style.backgroundColor = 'yellow';
+para.style.backgroundColor = prompt('Entrez une couleur');
+console.log(para.style.color);
+console.log(para.style.backgroundColor);
--- video_DOM/index.js
+++ video_DOM/index.js
@@ -3,138 +3,138 @@
 // const baliseHTML = document.querySelector("h4");
 
 // Click event
-const questionContainer = document.querySelector(".click-event");
-const btn1 = document.querySelector("#btn-1");
-const btn2 = document.getElementById("btn-2");
-const response = document.querySelector("p");
+const questionContainer = document.querySelector('.click-event');
+const btn1 = document.querySelector('#btn-1');
+const btn2 = document.getElementById('btn-2');
+const response = document.querySelector('p');
 
-questionContainer.addEventListener("click", () => {
-  questionContainer.classList.toggle("question-clicked");
+questionContainer.addEventListener('click', () => {
+  questionContainer.classList.toggle('question-clicked');
 });
 
-btn1.addEventListener("click", () => {
-  response.classList.add("show-response");
-  response.style.background = "green";
+btn1.addEventListener('click', () => {
+  response.classList.add('show-response');
+  response.style.background = 'green';
 });
 
-btn2.addEventListener("click", () => {
-  response.classList.add("show-response");
-  response.style.background = "red";
+btn2.addEventListener('click', () => {
+  response.classList.add('show-response');
+  response.style.background = 'red';
 });
 
 /* <div> > #id > .class > baliseHTML */
 
 //--------------------------------------------------
 // Mouse Events
-const mousemove = document.querySelector(".mousemove");
+const mousemove = document.querySelector('.mousemove');
 
-window.addEventListener("mousemove", (e) => {
-  mousemove.style.left = e.pageX + "px";
-  mousemove.style.top = e.pageY + "px";
+window.addEventListener('mousemove', (e) => {
+  mousemove.style.left = e.pageX + 'px';
+  mousemove.style.top = e.pageY + 'px';
 });
 
-window.addEventListener("mousedown", () => {
-  mousemove.style.transform = "scale(2) translate(-25%, -25%)";
+window.addEventListener('mousedown', () => {
+  mousemove.style.transform = 'scale(2) translate(-25%, -25%)';
 });
 
-window.addEventListener("mouseup", () => {
-  mousemove.style.transform = "scale(1) translate(-50%, -50%)";
-  mousemove.style.border = "2px solid teal";
+window.addEventListener('mouseup', () => {
+  mousemove.style.transform = 'scale(1) translate(-50%, -50%)';
+  mousemove.style.border = '2px solid teal';
 });
 
-questionContainer.addEventListener("mouseenter", () => {
-  questionContainer.style.background = "rgba(0,0,0,0.6)";
+questionContainer.addEventListener('mouseenter', () => {
+  questionContainer.style.background = 'rgba(0,0,0,0.6)';
 });
 
-questionContainer.addEventListener("mouseout", () => {
-  questionContainer.style.background = "pink";
+questionContainer.addEventListener('mouseout', () => {
+  questionContainer.style.background = 'pink';
 });
 
-response.addEventListener("mouseover", () => {
-  response.style.transform = "rotate(2deg)";
+response.addEventListener('mouseover', () => {
+  response.style.transform = 'rotate(2deg)';
 });
 
 //---------------------------------------------------
 // KeyPress event
 
-const keypressContainer = document.querySelector(".keypress");
-const key = document.getElementById("key");
+const keypressContainer = document.querySelector('.keypress');
+const key = document.getElementById('key');
 
 const ring = (key) => {
   const audio = new Audio();
-  audio.src = key + ".mp3";
+  audio.src = key + '.mp3';
   audio.play();
 };
 
-document.addEventListener("keypress", (e) => {
+document.addEventListener('keypress', (e) => {
   key.textContent = e.key;
 
-  if (e.key === "j") {
-    keypressContainer.style.background = "pink";
-  } else if (e.key === "h") {
-    keypressContainer.style.background = "teal";
+  if (e.key === 'j') {
+    keypressContainer.style.background = 'pink';
+  } else if (e.key === 'h') {
+    keypressContainer.style.background = 'teal';
   } else {
-    keypressContainer.style.background = "red";
+    keypressContainer.style.background = 'red';
   }
-  if (e.key === "z") ring(e.key);
+  if (e.key === 'z') ring(e.key);
 });
 
 //-------------------------------------------------
 // Scroll Event
 
-const nav = document.querySelector("nav");
+const nav = document.querySelector('nav');
 
-window.addEventListener("scroll", () => {
+window.addEventListener('scroll', () => {
   if (window.scrollY > 120) {
     nav.style.top = 0;
   } else {
-    nav.style.top = "-50px";
+    nav.style.top = '-50px';
   }
 });
 
 //--------------------------------------------------
 // Form Events
 const inputName = document.querySelector('input[type="text"]');
-const select = document.querySelector("select");
-const form = document.querySelector("form");
-let pseudo = "";
-let language = "";
+const select = document.querySelector('select');
+const form = document.querySelector('form');
+let pseudo = '';
+let language = '';
 
-inputName.addEventListener("input", (e) => {
+inputName.addEventListener('input', (e) => {
   pseudo = e.target.value;
 });
 
-select.addEventListener("input", (e) => {
+select.addEventListener('input', (e) => {
   language = e.target.value;
 });
 
-form.addEventListener("submit", (e) => {
+form.addEventListener('submit', (e) => {
   e.preventDefault();
 
   if (cgv.checked) {
-    document.querySelector("form > div").innerHTML = `
+    document.querySelector('form > div').innerHTML = `
       <h3>Pseudo : ${pseudo}</h3>
       <h4>Langage préféré : ${language}</h4>
     `;
   } else {
-    alert("Veuillez accepter les CGV");
+    alert('Veuillez accepter les CGV');
   }
 });
 
 //------------------------------------------------
 // Load event
-window.addEventListener("load", () => {
+window.addEventListener('load', () => {
   // console.log("Document Chargé !");
 });
 
 //------------------------------------------------
 // ForEach
 // const boxes = document.getElementsByClassName("box");
-const boxes = document.querySelectorAll(".box");
+const boxes = document.querySelectorAll('.box');
 
 boxes.forEach((box) => {
-  box.addEventListener("click", (e) => {
-    box.style.transform = "scale(0.7)";
+  box.addEventListener('click', (e) => {
+    box.style.transform = 'scale(0.7)';
   });
 });
 
@@ -145,15 +145,15 @@ boxes.forEach((box) => {
 // };
 
 // Bubbling => fin (de base l'eventlistener est paramétré en mode Bublbing)
-document.body.addEventListener("click", () => {
-  console.log("click 1 !");
+document.body.addEventListener('click', () => {
+  console.log('click 1 !');
 });
 
 // Usecapture
 document.body.addEventListener(
-  "click",
+  'click',
   () => {
-    console.log("click 2 !");
+    console.log('click 2 !');
   },
   true
 );
@@ -181,20 +181,20 @@ document.body.addEventListener(
 // alert("hello");
 
 // confirm
-btn2.addEventListener("click", () => {
-  confirm("Voulez vous vraiment vous tromper ?");
+btn2.addEventListener('click', () => {
+  confirm('Voulez vous vraiment vous tromper ?');
 });
 
 // prompt
-btn1.addEventListener("click", () => {
-  let answer = prompt("Entrez votre nom !");
+btn1.addEventListener('click', () => {
+  let answer = prompt('Entrez votre nom !');
 
-  questionContainer.innerHTML += "<h3>Bravo " + answer + "</h3>";
+  questionContainer.innerHTML += '<h3>Bravo ' + answer + '</h3>';
 });
 
 // Timer, compte à rebours
 setTimeout(() => {
-  questionContainer.style.borderRadius = "300px";
+  questionContainer.style.borderRadius = '300px';
 }, 2000);
 
 // let interval = setInterval(() => {
@@ -205,7 +205,7 @@ setTimeout(() => {
 //     `;
 // }, 4000);
 
-document.body.addEventListener("click", (e) => {
+document.body.addEventListener('click', (e) => {
   e.target.remove();
   clearInterval(interval);
 });
@@ -254,7 +254,7 @@ document.body.addEventListener("click", (e) => {
 
 //------------------------------------------------
 // SetProperty
-window.addEventListener("mousemove", (e) => {
-  nav.style.setProperty("--x", e.layerX + "px");
-  nav.style.setProperty("--y", e.layerY + "px");
+window.addEventListener('mousemove', (e) => {
+  nav.style.setProperty('--x', e.layerX + 'px');
+  nav.style.setProperty('--y', e.layerY + 'px');
 });
