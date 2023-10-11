8f3ed07edb6f1a2b7524c8cbe958d33fcac2cd7f
dialloabdoulaye848 <dialloabdoulaye848@gmail.com>
2023-10-11 15:28:31 +0000

    exo programmation asynchrone

--- Promess_js/promess_js/app.js
+++ Promess_js/promess_js/app.js
@@ -1,9 +1,28 @@
-//DECLARATION
-const calcul = (num1, num2) => {
+////devision
+const division = (a, b) => {
   return new Promise((resolve, reject) => {
-    //.............
-    const result = num1 * num2;
+    const result = a / b;
+    if (b !== 0) {
+      return resolve(`${result}`);
+    }
+    reject('calcul invalid');
+  });
+};
+division(30, 4)
+  .then((result) => result)
+  .then((result) => {
+    console.log('voici le resultat: ' + ' ' + result);
+  })
 
+  .catch((err) => {
+    console.log('Error:' + ' ' + err);
+  });
+
+//-----------  multiplication-----------------------
+
+const calcul = (a, b) => {
+  return new Promise((resolve, reject) => {
+    const result = a * b;
     if (result < 1000) {
       resolve(result);
     } else {
@@ -11,12 +30,75 @@ const calcul = (num1, num2) => {
     }
   });
 };
-
-// UTILISATION
 calcul(30, 9)
+  .then((result) => result)
   .then((result) => {
-    console.log('resultat: ' + ' ' + result);
+    console.log('voici le resultat: ' + ' ' + result);
   })
+
+  .catch((err) => {
+    console.log('Error:' + ' ' + err);
+  });
+
+//----------------addition------------------
+const addition = (a, b) => {
+  return new Promise((resolve, reject) => {
+    const result = a + b;
+    if (a > 0) {
+      return resolve(`${result}`);
+    } else {
+      reject('Erreur de calcul');
+    }
+  });
+};
+addition(30, 79)
+  .then((result) => result)
+  .then((result) => {
+    console.log('voici le resultat: ' + ' ' + result);
+  })
+
+  .catch((err) => {
+    console.log('Error:' + ' ' + err);
+  });
+
+//-------------------soustraction------
+const soustraction = (a, b) => {
+  return new Promise((resolve, reject) => {
+    const result = a - b;
+    if (a > b) {
+      return resolve(`${result}`);
+    } else {
+      reject('Erreur de calcul');
+    }
+  });
+};
+soustraction(80, 79)
+  .then((result) => result)
+  .then((result) => {
+    console.log('voici le resultat: ' + ' ' + result);
+  })
+
+  .catch((err) => {
+    console.log('Error:' + ' ' + err);
+  });
+
+//-----------Modulo--------------
+const modulo = (a, b) => {
+  return new Promise((resolve, reject) => {
+    const result = a % b;
+    if (a % b != 0) {
+      return resolve(`${result}`);
+    } else {
+      reject('Erreur de calcul');
+    }
+  });
+};
+modulo(123, 10)
+  .then((result) => result)
+  .then((result) => {
+    console.log('voici le resultat: ' + ' ' + result);
+  })
+
   .catch((err) => {
     console.log('Error:' + ' ' + err);
   });
